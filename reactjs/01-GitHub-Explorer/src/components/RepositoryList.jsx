15d3ced3f6fc232347c6dacaf32865676f62dcf8
Guilherme Henrique <guiihenriquecs@gmail.com>
2022-01-03 17:45:15 -0300

    hook useEffect and consuming github api

--- reactjs/01-GitHub-Explorer/src/components/RepositoryList.jsx
+++ reactjs/01-GitHub-Explorer/src/components/RepositoryList.jsx
@@ -1,24 +1,27 @@
 import { RepositoryItem } from "./RepositoryItem";
 
-const repository = {
-    name: 'Unform',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform'
-}
-    
+import '../styles/repositories.scss';
+import { useState, useEffect } from 'react';
 
+// https://api.github.com/users/ghenriquec/repos
 
 export function RepositoryList(){
-    return(
+   const [repositories, setRepositories] = useState([])
+   
+   useEffect(() => {
+    fetch('https://api.github.com/users/ghenriquec/repos')
+    .then(response => response.json())
+    .then(data => setRepositories(data))
+   }, []);
+
+  return(
       <section className="repository-list">
           <h1>Lista de repositórios</h1>
 
           <ul>
-            <RepositoryItem repository={repository}/>
-            <RepositoryItem repository={repository}/>
-            <RepositoryItem repository={repository}/>
-            <RepositoryItem repository={repository}/>
-            <RepositoryItem repository={repository}/>
+            {repositories.map(repository => {
+              return <RepositoryItem key={repository.name} repository={repository} />
+            })}
           </ul>
       </section>  
     );
